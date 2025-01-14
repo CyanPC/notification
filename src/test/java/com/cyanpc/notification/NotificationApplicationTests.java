2d7237d8bb2aad3041d1faf3f7c8d99d914548a6
Cyan Pereira <cyan.pereira@gmail.com>
2025-01-14 17:49:30 -0300

    1st commit

---
+++ src/test/java/com/cyanpc/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyanpc.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
